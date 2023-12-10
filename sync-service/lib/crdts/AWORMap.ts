1bd55d7c682de6d168ee2ce18da8546031012519
Guilherme Almeida <gui1612@sapo.pt>
2023-12-10 02:38:32 +0000

    fix: fixes AWORMap generics

--- sync-service/lib/crdts/AWORMap.ts
+++ sync-service/lib/crdts/AWORMap.ts
@@ -1,14 +1,18 @@
+import { AWSet } from "./AWSet";
 import { CCounter } from "./CCounter";
 import { DotContext } from "./DotContext";
+import { EWFlag } from "./EWFlag";
 
 type CRDT<K, T> = 
-    | AWORMap<K, T>
+    | AWORMap<K, CRDT<K, T>>
+    | AWSet<K, T>
     | CCounter<T>
+    | EWFlag<T>;
 
-class AWORMap<K, T> {
+class AWORMap<K, T extends CRDT<K, unknown>> {
     
     private id: T;
-    private map: Map<K, CRDT<K, T>> = new Map();
+    private map: Map<K, T> = new Map();
     private dotContext: DotContext<K> = new DotContext();
 
     constructor(parameters) {
@@ -16,3 +20,5 @@ class AWORMap<K, T> {
     }
     
 }
+
+export class { AWORMap }

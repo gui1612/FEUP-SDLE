1b751a3b545dfbb3681b0bca32dd9a2d0cc13815
Guilherme Almeida <gui1612@sapo.pt>
2023-12-13 02:22:39 +0000

    chore: removes useless files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -27,12 +27,12 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'favicon.ico',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'favicon.ico',
             sizes: '512x512',
             type: 'image/png'
           }
